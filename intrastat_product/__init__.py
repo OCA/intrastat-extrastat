468b53bdc089abf1a214007059031396bf93c16b
Luc De Meyer <luc.demeyer@noviat.com>
2015-07-25 17:18:44 +0200

    [IMP] intrastat_product: major update intrastat V3 modules
    * add region to intrastat_product

--- intrastat_product/__init__.py
+++ intrastat_product/__init__.py
@@ -1,7 +1,3 @@
 # -*- encoding: utf-8 -*-
 
-from . import res_company
-from . import intrastat
-from . import intrastat_declaration
-from . import stock
-from . import account_invoice
+from . import models

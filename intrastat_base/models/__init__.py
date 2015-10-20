fcab4ad9d396b03581ab9151575e12dd7f660a6e
luc-demeyer <luc.demeyer@noviat.com>
2015-10-20 20:26:19 +0200

    [REF] intrastat_base: major update intrastat V3

--- intrastat_base/models/__init__.py
+++ intrastat_base/models/__init__.py
@@ -20,4 +20,8 @@
 #
 ##############################################################################
 
-from . import models
+from . import res_country
+from . import product_template
+from . import account_tax
+from . import res_company
+from . import intrastat_common

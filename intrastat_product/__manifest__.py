6dfd0e5402444b039c02b1733f7733cc5232b7b4
Alexis de Lattre <alexis.delattre@akretion.com>
2016-10-04 00:41:33 +0200

    [MIG] intrastat_product: Migration to 10.0
    intrastat_base: Move company view params to account config page

---
+++ intrastat_product/__manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# © 2011-2017 Akretion (http://www.akretion.com)
+# © 2009-2017 Noviat (http://www.noviat.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# @author Luc de Meyer <info@noviat.com>
+
+{
+    'name': 'Intrastat Product',
+    'version': '10.0.1.0.0',
+    'category': 'Intrastat',
+    'license': 'AGPL-3',
+    'summary': 'Base module for Intrastat Product',
+    'author': 'Akretion, Noviat, Odoo Community Association (OCA)',
+    'depends': [
+        'intrastat_base',
+        'product_harmonized_system',
+        'sale_stock',
+        'purchase',
+        ],
+    'conflicts': ['report_intrastat'],
+    'data': [
+        'views/hs_code.xml',
+        'views/intrastat_region.xml',
+        'views/intrastat_unit.xml',
+        'views/intrastat_transaction.xml',
+        'views/intrastat_transport_mode.xml',
+        'views/intrastat_product_declaration.xml',
+        'views/account_config_settings.xml',
+        'views/account_invoice.xml',
+        'views/sale_order.xml',
+        'views/stock_warehouse.xml',
+        'security/intrastat_security.xml',
+        'security/ir.model.access.csv',
+        'data/intrastat_transport_mode.xml',
+        'data/intrastat_unit.xml',
+    ],
+    'demo': ['demo/intrastat_demo.xml'],
+    'installable': True,
+}
---
+++ intrastat_product/models/account_config_settings.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# © 2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields
+
+
+class AccountConfigSettings(models.TransientModel):
+    _inherit = 'account.config.settings'
+
+    intrastat_incoterm_id = fields.Many2one(
+        related='company_id.intrastat_incoterm_id')
+    intrastat_arrivals = fields.Selection(
+        related='company_id.intrastat_arrivals')
+    intrastat_dispatches = fields.Selection(
+        related='company_id.intrastat_dispatches')
+    intrastat = fields.Char(related='company_id.intrastat', readonly=True)
+    intrastat_transport_id = fields.Many2one(
+        related='company_id.intrastat_transport_id')
+    intrastat_region_id = fields.Many2one(
+        related='company_id.intrastat_region_id')
+    intrastat_transaction_out_invoice = fields.Many2one(
+        related='company_id.intrastat_transaction_out_invoice')
+    intrastat_transaction_out_refund = fields.Many2one(
+        related='company_id.intrastat_transaction_out_refund')
+    intrastat_transaction_in_invoice = fields.Many2one(
+        related='company_id.intrastat_transaction_in_invoice')
+    intrastat_transaction_in_refund = fields.Many2one(
+        related='company_id.intrastat_transaction_in_refund')
+    intrastat_accessory_costs = fields.Boolean(
+        related='company_id.intrastat_accessory_costs')

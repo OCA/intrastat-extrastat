5d8c60c5c4f0ed1379dd450ffeab69591970b3d2
Alexis de Lattre <alexis.delattre@akretion.com>
2016-10-04 00:41:33 +0200

    [MIG] intrastat_product: Migration to 10.0
    intrastat_base: Move company view params to account config page

--- intrastat_product/models/sale_order.py
+++ intrastat_product/models/sale_order.py
@@ -1,37 +1,26 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Intrastat Product module for Odoo
-#    Copyright (C) 2010-2015 Akretion (http://www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2010-2017 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
 
-from openerp import models, api
+from odoo import models, fields, api
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    @api.model
-    def _prepare_invoice(self, order, lines):
+    intrastat_transport_id = fields.Many2one(
+        'intrastat.transport_mode', string='Transport Mode',
+        help="This information is used in Intrastat reports")
+    intrastat = fields.Selection(
+        string='Intrastat Declaration',
+        related='company_id.intrastat_dispatches', readonly=True)
+
+    @api.multi
+    def _prepare_invoice(self):
         '''Copy destination country to invoice'''
-        invoice_vals = super(SaleOrder, self)._prepare_invoice(
-            order, lines)
-        invoice_vals['src_dest_country_id'] = \
-            order.partner_shipping_id.country_id.id or False
-        invoice_vals['incoterm_id'] = order.incoterm.id or False
-        return invoice_vals
+        vals = super(SaleOrder, self)._prepare_invoice()
+        if self.intrastat_transport_id:
+            vals['intrastat_transport_id'] = self.intrastat_transport_id.id
+        if self.warehouse_id.region_id:
+            vals['src_dest_region_id'] = self.warehouse_id.region_id.id
+        return vals

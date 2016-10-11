bf35526b313bb2a5d84cf704ce66c58dfbca5b82
Alexis de Lattre <alexis.delattre@akretion.com>
2016-10-11 14:59:28 +0200

    [MIG] intrastat_base: Migration to v10
    * Update README.rst: switch to new intrastat project
    * Move Intrastat menu entry to Accounting > Report instead of Accounting > Report > PDF Reports (because intrastat reports are not in PDF !)

---
+++ intrastat_base/models/account_config_settings.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# © 2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields
+
+
+class AccountConfigSettings(models.TransientModel):
+    _inherit = 'account.config.settings'
+
+    intrastat_remind_user_ids = fields.Many2many(
+        related='company_id.intrastat_remind_user_ids')
--- intrastat_base/models/res_company.py
+++ intrastat_base/models/res_company.py
@@ -1,27 +1,9 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Intrastat base module for Odoo
-#    Copyright (C) 2013-2014 Akretion (http://www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2013-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields, api, _
-from openerp.exceptions import ValidationError
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 
 class ResCompany(models.Model):
@@ -30,7 +12,7 @@ class ResCompany(models.Model):
     intrastat_remind_user_ids = fields.Many2many(
         'res.users', column1='company_id', column2='user_id',
         string="Users Receiving the Intrastat Reminder",
-        help="List of OpenERP users who will receive a notification to "
+        help="List of Odoo users who will receive a notification to "
         "remind them about the Intrastat declaration.")
     intrastat_email_list = fields.Char(
         compute='_compute_intrastat_email_list',

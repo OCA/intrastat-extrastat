202b527aa586b35de1d1ea66834500b343513279
Denis Leemann <denis.leemann@camptocamp.com>
2017-07-27 10:45:56 +0200

    Add hs_code_link

---
+++ hs_code_link/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "HS Code Link",
+    "version": "10.0.1.0.0",
+    "depends": [
+        'product_harmonized_system',
+        'delivery',
+    ],
+    "author": "Camptocamp SA, Odoo Community Association (OCA)",
+    "website": "http://www.camptocamp.com",
+    "license": "AGPL-3",
+    "category": "Reporting",
+    "data": [
+        'views/product_views.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}

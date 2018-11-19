ee006e8d20f4a5ed091e8254e5055e3d2ed22689
lfreeke <lfreeke@therp.nl>
2018-11-19 16:51:42 +0100

    [ADD] Module to add EORI number to company form

---
+++ company_eori_number/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "company_eori_number",
+    "version": "8.0.1.0.0",
+    "author": "Therp BV,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "",
+    "summary": "",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "views/res_company.xml",
+    ],
+    "auto_install": False,
+    "installable": True,
+    "application": False,
+}

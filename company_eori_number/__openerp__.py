7d8c258b46c8d5cdfaaf8ef54e2c7d5e6b7eaa11
Luc De Meyer <luc.demeyer@noviat.com>
2018-11-25 16:57:12 +0100

    Update __openerp__.py, cf. comments daramousk

--- company_eori_number/__openerp__.py
+++ company_eori_number/__openerp__.py
@@ -2,12 +2,12 @@
 # Copyright 2018 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
-    "name": "company_eori_number",
+    "name": "Company EORI number",
     "version": "8.0.1.0.0",
     "author": "Therp BV,Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "category": "",
-    "summary": "",
+    "category": "Extrastat",
+    "summary": "Add EORI number to company form",
     "depends": [
         "base",
     ],

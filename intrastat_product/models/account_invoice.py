d505851faa0366bbbcbbb9394f89a31a222468a2
Luc De Meyer <luc.demeyer@noviat.com>
2019-04-17 20:48:49 +0200

    [MIG] intrastat_product: Migration to 11.0
    * Small fixes in intrastat_base
    * account_tax_template

--- intrastat_product/models/account_invoice.py
+++ intrastat_product/models/account_invoice.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
-# © 2011-2017 Akretion (http://www.akretion.com)
-# © 2009-2017 Noviat (http://www.noviat.com)
+# Copyright 2011-2017 Akretion (http://www.akretion.com)
+# Copyright 2009-2018 Noviat (http://www.noviat.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # @author Luc de Meyer <info@noviat.com>
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class AccountInvoice(models.Model):
@@ -13,31 +12,34 @@ class AccountInvoice(models.Model):
     # in v10, the sale_stock module defines an incoterms_id
     # Odoo v8 name: incoterm_id
     intrastat_transaction_id = fields.Many2one(
-        'intrastat.transaction', string='Intrastat Transaction Type',
-        default=lambda self: self._default_intrastat_transaction_id(),
-        ondelete='restrict',
+        comodel_name='intrastat.transaction',
+        string='Intrastat Transaction Type',
+        ondelete='restrict', track_visibility='onchange',
         help="Intrastat nature of transaction")
     intrastat_transport_id = fields.Many2one(
-        'intrastat.transport_mode', string='Intrastat Transport Mode',
+        comodel_name='intrastat.transport_mode',
+        string='Intrastat Transport Mode',
         ondelete='restrict')
     src_dest_country_id = fields.Many2one(
-        'res.country', string='Origin/Destination Country',
+        comodel_name='res.country',
+        string='Origin/Destination Country',
         compute='_compute_intrastat_country',
-        store=True,
+        store=True, compute_sudo=True,
         help="Destination country for dispatches. Origin country for "
         "arrivals.")
     intrastat_country = fields.Boolean(
-        compute='_compute_intrastat_country',
-        store=True, string='Intrastat Country', readonly=True)
+        compute='_compute_intrastat_country', string='Intrastat Country',
+        store=True, readonly=True, compute_sudo=True)
     src_dest_region_id = fields.Many2one(
-        'intrastat.region', string='Origin/Destination Region',
+        comodel_name='intrastat.region',
+        string='Origin/Destination Region',
         default=lambda self: self._default_src_dest_region_id(),
         help="Origin region for dispatches, destination region for "
         "arrivals. This field is used for the Intrastat Declaration.",
         ondelete='restrict')
     intrastat = fields.Char(
         string='Intrastat Declaration',
-        related='company_id.intrastat', readonly=True)
+        related='company_id.intrastat', readonly=True, compute_sudo=True)
 
     @api.multi
     @api.depends('partner_shipping_id.country_id', 'partner_id.country_id')
@@ -51,25 +53,9 @@ class AccountInvoice(models.Model):
             inv.intrastat_country = country.intrastat
 
     @api.model
-    def _default_intrastat_transaction_id(self):
-        rco = self.env['res.company']
-        company = rco._company_default_get()
-        inv_type = self._context.get('type')
-        if inv_type == 'out_invoice':
-            return company.intrastat_transaction_out_invoice
-        elif inv_type == 'out_refund':
-            return company.intrastat_transaction_out_refund
-        elif inv_type == 'in_invoice':
-            return company.intrastat_transaction_in_invoice
-        elif inv_type == 'in_refund':
-            return company.intrastat_transaction_in_refund
-        else:
-            return self.env['intrastat.transaction']
-
-    @api.model
     def _default_src_dest_region_id(self):
         rco = self.env['res.company']
-        company = rco._company_default_get()
+        company = rco._company_default_get('account.invoice')
         return company.intrastat_region_id
 
 
@@ -77,7 +63,8 @@ class AccountInvoiceLine(models.Model):
     _inherit = 'account.invoice.line'
 
     hs_code_id = fields.Many2one(
-        'hs.code', string='Intrastat Code', ondelete='restrict')
+        comodel_name='hs.code',
+        string='Intrastat Code', ondelete='restrict')
 
     @api.onchange('product_id')
     def intrastat_product_id_change(self):
--- intrastat_product/models/intrastat_transaction.py
+++ intrastat_product/models/intrastat_transaction.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
-# © 2011-2017 Akretion (http://www.akretion.com)
-# © 2009-2017 Noviat (http://www.noviat.com)
+# Copyright 2011-2017 Akretion (http://www.akretion.com)
+# Copyright 2009-2018 Noviat (http://www.noviat.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # @author Luc de Meyer <info@noviat.com>
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class IntrastatTransaction(models.Model):
@@ -12,6 +11,10 @@ class IntrastatTransaction(models.Model):
     _description = "Intrastat Transaction"
     _order = 'code'
     _rec_name = 'display_name'
+    _sql_constraints = [(
+        'intrastat_transaction_code_unique',
+        'UNIQUE(code, company_id)',
+        'Code must be unique.')]
 
     code = fields.Char(string='Code', required=True)
     description = fields.Text(string='Description')
@@ -19,8 +22,9 @@ class IntrastatTransaction(models.Model):
         compute='_compute_display_name_field', string="Display Name",
         readonly=True, store=True)
     company_id = fields.Many2one(
-        'res.company', string='Company',
-        default=lambda self: self.env['res.company']._company_default_get())
+        comodel_name='res.company', string='Company',
+        default=lambda self: self.env['res.company']._company_default_get(
+            'intrastat.transaction'))
 
     @api.multi
     @api.depends('code', 'description')
@@ -32,8 +36,3 @@ class IntrastatTransaction(models.Model):
             this.display_name = len(display_name) > 55 \
                 and display_name[:55] + '...' \
                 or display_name
-
-    _sql_constraints = [(
-        'intrastat_transaction_code_unique',
-        'UNIQUE(code, company_id)',
-        'Code must be unique.')]

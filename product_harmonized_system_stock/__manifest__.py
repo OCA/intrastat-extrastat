9be4efaec111c26b44d89f71f2ffa48a7fae27b4
Alexis de Lattre <alexis.delattre@akretion.com>
2019-05-28 19:09:32 +0200

    [MIG] Migrate all modules from v11 to v12

---
+++ product_harmonized_system_stock/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Product Harmonized System (menu entry)',
+    'version': '12.0.1.0.0',
+    'category': 'Reporting',
+    'license': 'AGPL-3',
+    'summary': 'Adds a menu entry for H.S. codes',
+    'author': 'Akretion, Odoo Community Association (OCA)',
+    'depends': ['product_harmonized_system', 'stock'],
+    'data': [
+        'views/hs_code_menu.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}

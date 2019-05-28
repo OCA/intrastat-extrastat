c2ac174fef82af4ef46fae3e319a33a19075f27c
Alexis de Lattre <alexis.delattre@akretion.com>
2019-05-28 19:09:32 +0200

    [MIG] Migrate all modules from v11 to v12
    [UPD] Update intrastat_base.pot

--- intrastat_base/models/res_country.py
+++ intrastat_base/models/res_country.py
@@ -1,4 +1,5 @@
-# Copyright 2011-2014 Akretion (<alexis.delattre@akretion.com>)
+# Copyright 2011-2019 Akretion France (http://www.akretion.com/)
+# @author: <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models

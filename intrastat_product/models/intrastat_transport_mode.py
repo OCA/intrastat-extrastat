61b59d9237096efb994f9f8916dee6398a4685b9
Alexis de Lattre <alexis.delattre@akretion.com>
2019-05-28 19:09:32 +0200

    [MIG] Migrate all modules from v11 to v12

--- intrastat_product/models/intrastat_transport_mode.py
+++ intrastat_product/models/intrastat_transport_mode.py
@@ -9,22 +9,20 @@ from odoo import api, fields, models
 class IntrastatTransportMode(models.Model):
     _name = 'intrastat.transport_mode'
     _description = "Intrastat Transport Mode"
-    _rec_name = 'display_name'
     _order = 'code'
     _sql_constraints = [(
         'intrastat_transport_code_unique',
         'UNIQUE(code)',
         'Code must be unique.')]
 
-    display_name = fields.Char(
-        string='Display Name', compute='_compute_display_name', store=True,
-        readonly=True)
     code = fields.Char(string='Code', required=True)
     name = fields.Char(string='Name', required=True, translate=True)
     description = fields.Char(string='Description', translate=True)
 
-    @api.multi
     @api.depends('name', 'code')
-    def _compute_display_name(self):
-        for this in self:
-            this.display_name = '%s. %s' % (this.code, this.name)
+    def name_get(self):
+        res = []
+        for mode in self:
+            name = '%s. %s' % (mode.code, mode.name)
+            res.append((mode.id, name))
+        return res

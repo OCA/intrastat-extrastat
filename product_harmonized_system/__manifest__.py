10d9ca506a8cce20a7f4c88fa1505be0a7d85f0c
BenjaHe <benjamin.henquet@noviat.com>
2019-12-19 09:18:09 +0100

    [13.0] [MIG] product harmonized system (#80)
    * Large code re-engineering following the Akretion-Noviat code sprint on intrastat
    
    This code has been written both by Luc de Meyer and myself.
    
    * hs_code display_name fix
    
    * major update intrastat V3 modules
    
    * Add product_origin_country_id on declaration/computation lines
    
    Copy incoterms and destination country from SO to invoice when invoicing from SO
    We need weight even when supplementary units is used
    Small cleanups and enhancements
    
    * Add support for accessory costs
    
    Add default values for intrastat transaction on company
    Code cleanup
    
    * this week's ci errors
    
    * Update README.rst
    
    Rename variables
    
    * Port intrastat_base to v10
    
    Set 2 other modules to uninstallable
    Update README.rst: switch to new intrastat project
    
    * Port product_harmonized_system to v10
    
    * Fix warning on display_name
    
    Special thanks to Pedro and Holger for finding the solution for display_name
    Fix demo data
    
    * [MIG] product_harmonized_system: Migration to 11.0
    
    * [FIX] product_harmonized_system: fix get_hs_code_recurively
    
    * [IMP] *: removed utf-8 headers, changed icon and site
    
    * company model test, country model test as in v8
    * png logo instead of svg, removed Python 2 utf-8 headers
    
    * [UPD] Update product_harmonized_system.pot
    
    * Add new module product_harmonized_system_delivery (hide native hs_code field)
    
    Finalize port to v11
    
    * [FIX+IMP] intrastat_*: Several things:
    
    * Add readonly on some fields when state of declaration is done
    * Division by zero in computation of accessory costs
    * Add FR translation
    * Fix strings
    * Minor code updates
    
    * [MIG] intrastat_product: Migration to 11.0
    
    * Small fixes in intrastat_base
    * account_tax_template
    
    * [ADD] icon.png
    
    * [UPD] Update product_harmonized_system.pot
    
    * Prepare v12 branch
    
    * [MIG] Migrate all modules from v11 to v12
    
    * flake8
    
    * [UPD] Update product_harmonized_system.pot
    
    * Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: intrastat-extrastat-12.0/intrastat-extrastat-12.0-product_harmonized_system
    Translate-URL: https://translation.odoo-community.org/projects/intrastat-extrastat-12-0/intrastat-extrastat-12-0-product_harmonized_system/
    
    * [FIX] product_harmized_system DESCRIPTION.rst
    
    * [UPD] README.rst
    
    * [UPD] README.rst
    
    * Added translation using Weblate (Spanish)
    
    * Translated using Weblate (Spanish)
    
    Currently translated at 50.0% (18 of 36 strings)
    
    Translation: intrastat-extrastat-12.0/intrastat-extrastat-12.0-product_harmonized_system
    Translate-URL: https://translation.odoo-community.org/projects/intrastat-extrastat-12-0/intrastat-extrastat-12-0-product_harmonized_system/es/
    
    * [MIG] product_harmonized_system : Migration to 13.0
    
    * [IMP] product_harmonized_system: black, isort

---
+++ product_harmonized_system/__manifest__.py
@@ -0,0 +1,28 @@
+# Copyright 2018 brain-tec AG (http://www.braintec-group.com)
+# Copyright 2011-2016 Akretion (http://www.akretion.com)
+# Copyright 2009-2019 Noviat (http://www.noviat.com)
+# @author Benjamin Henquet <info@noviat.com>
+# @author Kumar Aberer <kumar.aberer@braintec-group.com>
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# @author Luc de Meyer <info@noviat.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Harmonized System Codes",
+    "version": "13.0.1.0.0",
+    "category": "Reporting",
+    "license": "AGPL-3",
+    "summary": "Base module for Product Import/Export reports",
+    "author": "brain-tec AG, Akretion, Noviat, " "Odoo Community Association (OCA)",
+    "depends": ["product"],
+    "excludes": ["account_intrastat"],
+    "data": [
+        "security/product_hs_security.xml",
+        "security/ir.model.access.csv",
+        "views/hs_code.xml",
+        "views/product_category.xml",
+        "views/product_template.xml",
+    ],
+    "demo": ["demo/product_demo.xml"],
+    "installable": True,
+}

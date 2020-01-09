2ddb0d20f03febf8a6ddfa78908ba750ce245d3c
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-01-09 08:04:08 +0100

    [REF] product_harmonized_system_delivery: Black python code

--- product_harmonized_system_delivery/__manifest__.py
+++ product_harmonized_system_delivery/__manifest__.py
@@ -3,15 +3,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Product Harmonized System Codes - Delivery',
-    'version': '12.0.1.0.0',
-    'category': 'Reporting',
-    'license': 'AGPL-3',
-    'summary': 'Hide native hs_code field provided by the delivery_hs_code '
-               'module',
-    'author': 'Akretion, Odoo Community Association (OCA)',
-    'depends': ['delivery_hs_code', 'product_harmonized_system'],
-    'data': ['views/product_template.xml'],
-    'installable': True,
-    'auto_install': True,
+    "name": "Product Harmonized System Codes - Delivery",
+    "version": "12.0.1.0.0",
+    "category": "Reporting",
+    "license": "AGPL-3",
+    "summary": "Hide native hs_code field provided by the delivery_hs_code "
+    "module",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "depends": ["delivery_hs_code", "product_harmonized_system"],
+    "data": ["views/product_template.xml"],
+    "installable": True,
+    "auto_install": True,
 }

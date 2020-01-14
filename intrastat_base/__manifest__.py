371ca878828836eaa8af3dd8974d8639356b4c9d
Luc De Meyer <luc.demeyer@noviat.com>
2020-01-14 18:15:30 +0100

    [MIG] intrastat_base: Migration to 13.0

--- intrastat_base/__manifest__.py
+++ intrastat_base/__manifest__.py
@@ -1,11 +1,11 @@
 # Copyright 2011-2016 Akretion (http://www.akretion.com)
 # Copyright 2018 brain-tec AG (Kumar Aberer <kumar.aberer@braintec-group.com>)
-# Copyright 2009-2019 Noviat (http://www.noviat.com)
+# Copyright 2009-2020 Noviat (http://www.noviat.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Intrastat Reporting Base',
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     'category': 'Intrastat',
     'license': 'AGPL-3',
     'summary': 'Base module for Intrastat reporting',

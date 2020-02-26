ef1170cd51c4fa70be4ba82da8e77885cfb2ed3b
Luc De Meyer <luc.demeyer@noviat.com>
2020-02-26 22:16:50 +0100

    [MIG] intrastat_product: Migration to 13.0

--- intrastat_product/models/intrastat_transport_mode.py
+++ intrastat_product/models/intrastat_transport_mode.py
@@ -7,22 +7,21 @@ from odoo import api, fields, models
 
 
 class IntrastatTransportMode(models.Model):
-    _name = 'intrastat.transport_mode'
+    _name = "intrastat.transport_mode"
     _description = "Intrastat Transport Mode"
-    _order = 'code'
-    _sql_constraints = [(
-        'intrastat_transport_code_unique',
-        'UNIQUE(code)',
-        'Code must be unique.')]
+    _order = "code"
+    _sql_constraints = [
+        ("intrastat_transport_code_unique", "UNIQUE(code)", "Code must be unique.")
+    ]
 
-    code = fields.Char(string='Code', required=True)
-    name = fields.Char(string='Name', required=True, translate=True)
-    description = fields.Char(string='Description', translate=True)
+    code = fields.Char(string="Code", required=True)
+    name = fields.Char(string="Name", required=True, translate=True)
+    description = fields.Char(string="Description", translate=True)
 
-    @api.depends('name', 'code')
+    @api.depends("name", "code")
     def name_get(self):
         res = []
         for mode in self:
-            name = '%s. %s' % (mode.code, mode.name)
+            name = "{}. {}".format(mode.code, mode.name)
             res.append((mode.id, name))
         return res

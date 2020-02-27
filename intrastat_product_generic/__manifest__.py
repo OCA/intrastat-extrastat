ce07bac3210644183e75ce456b4f9f5877cac943
Luc De Meyer <luc.demeyer@noviat.com>
2020-02-27 22:11:12 +0100

    intrastat_product_generic

--- intrastat_product_generic/__manifest__.py
+++ intrastat_product_generic/__manifest__.py
@@ -1,20 +1,14 @@
-# Copyright 2009-2018 Noviat.
+# Copyright 2009-2020 Noviat.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Generic Intrastat Product Declaration',
-    'version': '12.0.1.0.0',
-    'category': 'Accounting & Finance',
-    'website': 'https://github.com/OCA/intrastat',
-    'author': 'Noviat,'
-              'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'installable': True,
-    'depends': [
-        'intrastat_product',
-    ],
-    'data': [
-        'security/intrastat_security.xml',
-        'views/intrastat_product.xml'
-    ],
+    "name": "Generic Intrastat Product Declaration",
+    "version": "13.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://github.com/OCA/intrastat",
+    "author": "Noviat," "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["intrastat_product"],
+    "data": ["security/intrastat_security.xml", "views/intrastat_product.xml"],
 }

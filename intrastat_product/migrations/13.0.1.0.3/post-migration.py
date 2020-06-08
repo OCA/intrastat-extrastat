dd4261a9b867f0c3c7d50dd579f875429a71c0b9
Alexis de Lattre <alexis.delattre@akretion.com>
2020-06-08 22:39:46 +0200

    intrastat_product: Add xml_attachment_id field
    intrastat_product: add mail.activity.mixin and widget in form view
    product_harmonized_system: remove archive button (use Action menu)
    All modules:
    - use super() instead of super(class, self)
    - Update domain_force in multi-company ir.rule to v13 standard

---
+++ intrastat_product/migrations/13.0.1.0.3/post-migration.py
@@ -0,0 +1,10 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade  # pylint: disable=W7936
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(
+        env.cr, "intrastat_product", "migrations/13.0.1.0.3/noupdate_changes.xml"
+    )

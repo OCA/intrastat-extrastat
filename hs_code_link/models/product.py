65871b86d7aebf1f9c443949b263b73e26844268
mreficent <miquel.raich@eficent.com>
2020-09-29 16:04:44 +0200

    [IMP] hs_code_link: black, isort, prettier

--- hs_code_link/models/product.py
+++ hs_code_link/models/product.py
@@ -1,15 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
-    hs_code = fields.Char(
-        related='hs_code_id.hs_code',
-        readonly=True,
-        store=True,
-    )
+    hs_code = fields.Char(related="hs_code_id.hs_code", readonly=True, store=True)

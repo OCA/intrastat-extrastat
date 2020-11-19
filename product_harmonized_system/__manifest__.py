ca29227e46d89376bcf4f55c574d0a475e2526df
Alexis de Lattre <alexis.delattre@akretion.com>
2020-11-19 18:49:45 +0100

    [MIG] product_harmonized_system to v14
    [UPD] README.rst
    
    [UPD] Update product_harmonized_system.pot
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: intrastat-extrastat-14.0/intrastat-extrastat-14.0-product_harmonized_system
    Translate-URL: https://translation.odoo-community.org/projects/intrastat-extrastat-14-0/intrastat-extrastat-14-0-product_harmonized_system/

--- product_harmonized_system/__manifest__.py
+++ product_harmonized_system/__manifest__.py
@@ -1,6 +1,6 @@
-# Copyright 2018 brain-tec AG (http://www.braintec-group.com)
-# Copyright 2011-2016 Akretion (http://www.akretion.com)
-# Copyright 2009-2019 Noviat (http://www.noviat.com)
+# Copyright 2018-2020 brain-tec AG (http://www.braintec-group.com)
+# Copyright 2011-2020 Akretion (http://www.akretion.com)
+# Copyright 2009-2020 Noviat (http://www.noviat.com)
 # @author Benjamin Henquet <info@noviat.com>
 # @author Kumar Aberer <kumar.aberer@braintec-group.com>
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
@@ -9,11 +9,13 @@
 
 {
     "name": "Product Harmonized System Codes",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "category": "Reporting",
     "license": "AGPL-3",
     "summary": "Base module for Product Import/Export reports",
     "author": "brain-tec AG, Akretion, Noviat, " "Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via", "luc-demeyer"],
+    "website": "https://github.com/OCA/intrastat-extrastat",
     "depends": ["product"],
     "excludes": ["account_intrastat"],
     "data": [
--- product_harmonized_system/models/product_category.py
+++ product_harmonized_system/models/product_category.py
@@ -1,5 +1,5 @@
-# Copyright 2011-2016 Akretion France (http://www.akretion.com)
-# Copyright 2009-2016 Noviat (http://www.noviat.com)
+# Copyright 2011-2020 Akretion France (http://www.akretion.com)
+# Copyright 2009-2020 Noviat (http://www.noviat.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # @author Luc de Meyer <info@noviat.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).

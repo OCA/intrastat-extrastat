58bffd2d5c7b7db230fa645d0ea58cb45d1e88d0
Luc De Meyer <luc.demeyer@noviat.com>
2020-12-11 18:05:26 +0100

    [13.0]invoice_date -> date

--- intrastat_product/__manifest__.py
+++ intrastat_product/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "Intrastat Product",
-    "version": "13.0.1.1.1",
+    "version": "13.0.1.1.2",
     "category": "Intrastat",
     "license": "AGPL-3",
     "summary": "Base module for Intrastat Product",

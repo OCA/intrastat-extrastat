c5a58118da29d79b57843cfa054ed6715c7de5f4
mreficent <miquel.raich@eficent.com>
2020-12-11 15:09:32 +0100

    [FIX] Migration fixes + full migration scripts

--- intrastat_product/__manifest__.py
+++ intrastat_product/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "Intrastat Product",
-    "version": "13.0.1.1.0",
+    "version": "13.0.1.1.1",
     "category": "Intrastat",
     "license": "AGPL-3",
     "summary": "Base module for Intrastat Product",

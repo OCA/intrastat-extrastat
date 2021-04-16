aecfe90fbe55c5d55d098710ce371792a6122d63
Holger Brunn <mail@hunki-enterprises.com>
2021-04-16 16:18:23 +0200

    [MIG] hs_code_link to v12

--- hs_code_link/__manifest__.py
+++ hs_code_link/__manifest__.py
@@ -2,8 +2,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "HS Code Link",
-    "version": "13.0.1.0.1",
-    "depends": ["product_harmonized_system", "delivery"],
+    "version": "12.0.1.0.0",
+    "depends": ["product_harmonized_system", "delivery_hs_code"],
     "author": "Camptocamp SA, Odoo Community Association (OCA)",
     "website": "http://www.camptocamp.com",
     "license": "AGPL-3",

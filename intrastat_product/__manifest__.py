9e9c965fe1305cbd0e6dbac47e3e1e366a295df6
João Marques <joao.marques@tecnativa.com>
2021-05-10 09:15:10 +0100

    [FIX] intrastat_product: don't force wrong domain in moves
    The intrastat transaction domain is already forced in the model and in the field in account.move.
    It was being incorrectly forced in the move, where it needed to be associated to the same company as the record, but those types can have no company associated
    
    TT29582
    
    intrastat_product 14.0.1.1.0

--- intrastat_product/__manifest__.py
+++ intrastat_product/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "Intrastat Product",
-    "version": "14.0.1.0.1",
+    "version": "14.0.1.1.0",
     "category": "Intrastat",
     "license": "AGPL-3",
     "summary": "Base module for Intrastat Product",

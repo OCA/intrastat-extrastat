2189ec1ccb780482ba3a06894d38470624e25c78
Reyes4711 <carlos.rg.92.47@gmail.com>
2021-05-17 15:28:26 +0200

    [14.0][MIG] hs_code_link: Migration to 14.0

--- hs_code_link/__manifest__.py
+++ hs_code_link/__manifest__.py
@@ -1,12 +1,12 @@
 # Copyright 2017 Camptocamp SA
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "HS Code Link",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "depends": ["product_harmonized_system", "delivery"],
     "author": "Camptocamp SA, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/intrastat-extrastat",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "category": "Reporting",
     "data": ["views/product_views.xml"],
     "installable": True,

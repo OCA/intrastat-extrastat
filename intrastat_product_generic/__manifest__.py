972f9169c3fab21901ebda8f8e58a42b82423b7d
Denis Roussel <denis.roussel@acsone.eu>
2021-10-20 11:28:46 +0200

    [14.0][MIG] intrastat_product_generic

--- intrastat_product_generic/__manifest__.py
+++ intrastat_product_generic/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Generic Intrastat Product Declaration",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Accounting & Finance",
     "website": "https://github.com/OCA/intrastat-extrastat",
     "author": "Noviat," "Odoo Community Association (OCA)",

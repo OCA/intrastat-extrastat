b4374c4c238f69ec2091de7437aed10a9d7e2a05
Denis Roussel <denis.roussel@acsone.eu>
2021-10-23 18:46:52 +0200

    [14.0][IMP] intrastat_*: Improve tests and code

--- intrastat_base/models/intrastat_common.py
+++ intrastat_base/models/intrastat_common.py
@@ -10,7 +10,7 @@ from traceback import format_exception
 from lxml import etree
 
 from odoo import _, api, fields, models, tools
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError, ValidationError
 
 logger = logging.getLogger(__name__)
 
@@ -19,6 +19,90 @@ class IntrastatCommon(models.AbstractModel):
     _name = "intrastat.common"
     _description = "Common functions for intrastat reports for products "
     "and services"
+
+    company_id = fields.Many2one(
+        comodel_name="res.company",
+        string="Company",
+        required=True,
+        states={"done": [("readonly", True)]},
+        default=lambda self: self._default_company_id(),
+    )
+    company_country_code = fields.Char(
+        compute="_compute_company_country_code",
+        string="Company Country Code",
+        readonly=True,
+        store=True,
+        help="Used in views and methods of localization modules.",
+    )
+    state = fields.Selection(
+        selection=[("draft", "Draft"), ("done", "Done")],
+        string="State",
+        readonly=True,
+        tracking=True,
+        copy=False,
+        default="draft",
+        help="State of the declaration. When the state is set to 'Done', "
+        "the parameters become read-only.",
+    )
+    note = fields.Text(
+        string="Notes", help="You can add some comments here if you want."
+    )
+
+    year = fields.Char(
+        string="Year", required=True, states={"done": [("readonly", True)]}
+    )
+    month = fields.Selection(
+        selection=[
+            ("01", "01"),
+            ("02", "02"),
+            ("03", "03"),
+            ("04", "04"),
+            ("05", "05"),
+            ("06", "06"),
+            ("07", "07"),
+            ("08", "08"),
+            ("09", "09"),
+            ("10", "10"),
+            ("11", "11"),
+            ("12", "12"),
+        ],
+        string="Month",
+        required=True,
+        states={"done": [("readonly", True)]},
+    )
+    year_month = fields.Char(
+        compute="_compute_year_month",
+        string="Period",
+        readonly=True,
+        tracking=True,
+        store=True,
+        help="Year and month of the declaration.",
+    )
+
+    @api.model
+    def _default_company_id(self):
+        return self.env.company
+
+    @api.depends("company_id")
+    def _compute_company_country_code(self):
+        for this in self:
+            if this.company_id:
+                if not this.company_id.country_id:
+                    raise ValidationError(_("You must set company's country !"))
+                this.company_country_code = this.company_id.country_id.code.lower()
+
+    @api.depends("year", "month")
+    def _compute_year_month(self):
+        for this in self:
+            if this.year and this.month:
+                this.year_month = "-".join([this.year, this.month])
+
+    @api.model
+    @api.constrains("year")
+    def _check_year(self):
+        for this in self:
+            if len(this.year) != 4 or this.year[0] != "2":
+                raise ValidationError(_("Invalid Year !"))
 
     # The method _compute_numbers has been removed
     # because it was using a loop on lines, which is slow -> we should
--- intrastat_base/tests/test_all.py
+++ intrastat_base/tests/test_all.py
@@ -1,13 +1,70 @@
-from odoo.tests.common import TransactionCase
+from odoo.exceptions import UserError, ValidationError
+from odoo.tests.common import SavepointCase
+
+from .common import IntrastatCommon
 
 
-class TestIntrastatBase(TransactionCase):
+class TestIntrastatBase(IntrastatCommon):
     """Tests for this module"""
 
     def test_company(self):
         # add 'Demo user' to intrastat_remind_user_ids
-        demo_user = self.env.ref("base.user_demo")
-        demo_company = self.env.ref("base.main_company")
-        demo_company.write({"intrastat_remind_user_ids": [(6, False, [demo_user.id])]})
+        self.demo_company.write(
+            {"intrastat_remind_user_ids": [(6, False, [self.demo_user.id])]}
+        )
         # then check if intrastat_email_list contains the email of the user
-        self.assertEqual(demo_company.intrastat_email_list, demo_user.email)
+        self.assertEqual(self.demo_company.intrastat_email_list, self.demo_user.email)
+
+    def test_no_email(self):
+        self.demo_user.email = False
+        with self.assertRaises(ValidationError):
+            self.demo_company.write(
+                {"intrastat_remind_user_ids": [(6, False, [self.demo_user.id])]}
+            )
+
+    def test_accessory(self):
+        with self.assertRaises(ValidationError):
+            self.shipping_cost.type = "consu"
+
+    def test_declaration_no_country(self):
+        self.demo_company.country_id = False
+        with self.assertRaises(ValidationError):
+            self._create_declaration()
+            self.declaration.flush()
+
+    def test_declaration_no_vat(self):
+        self.demo_company.partner_id.vat = False
+        with self.assertRaises(UserError):
+            self._create_declaration()
+            self.declaration.flush()
+            self.declaration._check_generate_xml()
+
+    def test_declaration_send_mail(self):
+        self._create_declaration()
+        mail_before = self.mail_obj.search([])
+        self.declaration.send_reminder_email(self.mail_template_id)
+        mail_after = self.mail_obj.search([]) - mail_before
+        self.assertEqual(0, len(mail_after))
+        self.demo_company.write(
+            {"intrastat_remind_user_ids": [(6, False, [self.demo_user.id])]}
+        )
+        self.declaration.send_reminder_email(self.mail_template_id)
+        mail_after = self.mail_obj.search([]) - mail_before
+        self.assertEqual(1, len(mail_after))
+        self.assertIn(
+            mail_after.email_to,
+            self.demo_user.email,
+        )
+
+    def test_declaration_state(self):
+        self._create_declaration()
+        self.declaration.unlink()
+
+        self._create_declaration()
+        self.declaration.state = "done"
+        with self.assertRaises(UserError):
+            self.declaration.unlink()
+
+
+class TestIntrastat(TestIntrastatBase, SavepointCase):
+    """ Test Intrastat """

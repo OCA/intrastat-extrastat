3de2cf631c9c2c0438f55285160b3ca687c405b3
GuillemCForgeFlow <guillem.casassas@forgeflow.com>
2021-12-01 13:28:54 +0100

    [14.0][MIG]hs_code_link: Migration to 14.0

--- hs_code_link/__manifest__.py
+++ hs_code_link/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "HS Code Link",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "depends": ["product_harmonized_system", "delivery"],
     "author": "Camptocamp SA, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/intrastat-extrastat",

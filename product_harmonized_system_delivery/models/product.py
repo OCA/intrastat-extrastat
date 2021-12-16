1415704cc7f77ce24d0625ef6f5452ec5956b015
Alexis de Lattre <alexis.delattre@akretion.com>
2021-12-16 17:23:37 +0100

    product_harmonized_system_delivery: hs_code is now store=True
    This commit is the "followup" of the change to company_dependent=False
    on the fields hs_code_id on product.template and product.category.
    Add groupby on hs_code_id on product.template search view
    hs.code is now company_id=False by default

--- product_harmonized_system_delivery/models/product.py
+++ product_harmonized_system_delivery/models/product.py
@@ -7,5 +7,4 @@ from odoo import fields, models
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    # this field cannot be stored because hs_code_id is company dependent
-    hs_code = fields.Char(related="hs_code_id.hs_code", readonly=True, store=False)
+    hs_code = fields.Char(related="hs_code_id.hs_code", store=True)

89b37ecf599f6f1e8156a89c18ff8608142e75ad
Denis Roussel <denis.roussel@acsone.eu>
2022-03-30 10:57:18 +0200

    [14.0][IMP] intrastat_product: Add tests for Excel generation

--- intrastat_product/tests/test_sale_order.py
+++ intrastat_product/tests/test_sale_order.py
@@ -83,6 +83,14 @@ class TestIntrastatProductSale(IntrastatSaleCommon):
         self.declaration.generate_declaration()
         self._check_line_values(final=True)
 
+        # Check the Excel computation file
+        file_data = self._create_xls()
+        self.check_xls(file_data[0])
+
+        # Check the Excel declaration file
+        file_data = self._create_xls(True)
+        self.check_xls(file_data[0], True)
+
 
 class TestIntrastatProductSaleCase(TestIntrastatProductSale, SavepointCase):
     """Test Intrastat Sale"""

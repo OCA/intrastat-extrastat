6a1e3c0e2b468b32dedd5dca4434c68989e03cf6
Luc De Meyer <luc.demeyer@noviat.com>
2022-05-16 12:14:02 +0200

    [14.0]fix stack trace when running module upgrade on 14.0.1.6.0
    [UPD] Update intrastat_product.pot
    
    intrastat_product 14.0.1.6.1
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: intrastat-extrastat-14.0/intrastat-extrastat-14.0-intrastat_product
    Translate-URL: https://translation.odoo-community.org/projects/intrastat-extrastat-14-0/intrastat-extrastat-14-0-intrastat_product/

--- intrastat_product/__manifest__.py
+++ intrastat_product/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "Intrastat Product",
-    "version": "14.0.1.6.0",
+    "version": "14.0.1.6.1",
     "category": "Intrastat",
     "license": "AGPL-3",
     "summary": "Base module for Intrastat Product",

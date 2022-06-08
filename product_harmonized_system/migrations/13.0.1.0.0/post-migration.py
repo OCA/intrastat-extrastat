d8bc26afe4a88a5e27243966601637ad2d5bd427
Víctor Martínez <victor.martinez@tecnativa.com>
2022-06-08 09:26:13 +0200

    [FIX] product_harmonized_system: Multi-company rules migration script. TT37373

---
+++ product_harmonized_system/migrations/13.0.1.0.0/post-migration.py
@@ -0,0 +1,13 @@
+# Copyright 2022 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(
+        env.cr,
+        "product_harmonized_system",
+        "migrations/13.0.1.0.0/noupdate_changes.xml",
+    )

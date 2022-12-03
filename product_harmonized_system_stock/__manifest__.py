03980fc68455ed96c19973b93cc2344f1a165806
Alexis de Lattre <alexis.delattre@akretion.com>
2022-12-03 12:25:04 +0100

    [MIG] product_harmonized_system_stock to v15

--- product_harmonized_system_stock/__manifest__.py
+++ product_harmonized_system_stock/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2019-2020 Akretion France (http://www.akretion.com)
+# Copyright 2019-2022 Akretion France (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Product Harmonized System (menu entry)",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Reporting",
     "license": "AGPL-3",
     "summary": "Adds a menu entry for H.S. codes",

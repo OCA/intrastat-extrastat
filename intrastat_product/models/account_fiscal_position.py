cc10b0fe67868fad4e32583183d3db866cfbf724
Alexis de Lattre <alexis.delattre@akretion.com>
2023-01-23 18:57:14 +0100

    intrastat_product: default intrastat transaction on fiscal position AND company
    Odoo uses, by order of priority:
    1) intrastat transaction of the invoice
    2) default intrastat transaction of the fiscal position of the invoice
    3) default intrastat transaction of the company

--- intrastat_product/models/account_fiscal_position.py
+++ intrastat_product/models/account_fiscal_position.py
@@ -11,18 +11,34 @@ class AccountFiscalPosition(models.Model):
     intrastat_out_invoice_transaction_id = fields.Many2one(
         comodel_name="intrastat.transaction",
         string="Default Intrastat Transaction For Customer Invoice",
+        help="Odoo uses the intrastat transaction of the invoice. "
+        "If not set, Odoo uses the default intrastat transaction of "
+        "the fiscal position of the invoice. "
+        "If not set, Odoo uses the default intrastat transaction of the company.",
     )
     intrastat_out_refund_transaction_id = fields.Many2one(
         comodel_name="intrastat.transaction",
         string="Default Intrastat Transaction for Customer Refunds",
+        help="Odoo uses the intrastat transaction of the invoice. "
+        "If not set, Odoo uses the default intrastat transaction of "
+        "the fiscal position of the invoice. "
+        "If not set, Odoo uses the default intrastat transaction of the company.",
     )
     intrastat_in_invoice_transaction_id = fields.Many2one(
         comodel_name="intrastat.transaction",
         string="Default Intrastat Transaction For Supplier Invoices",
+        help="Odoo uses the intrastat transaction of the invoice. "
+        "If not set, Odoo uses the default intrastat transaction of "
+        "the fiscal position of the invoice. "
+        "If not set, Odoo uses the default intrastat transaction of the company.",
     )
     intrastat_in_refund_transaction_id = fields.Many2one(
         comodel_name="intrastat.transaction",
         string="Default Intrastat Transaction For Supplier Refunds",
+        help="Odoo uses the intrastat transaction of the invoice. "
+        "If not set, Odoo uses the default intrastat transaction of "
+        "the fiscal position of the invoice. "
+        "If not set, Odoo uses the default intrastat transaction of the company.",
     )
     # field used to show/hide fields in country-specific modules
     company_country_code = fields.Char(related="company_id.country_id.code")

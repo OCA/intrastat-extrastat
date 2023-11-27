f5c74332e13ac913dff181c0669a1c1d131c94d5
rosenvladimirov <rosen.vladimirov@mecproduction.eu>
2023-11-27 14:44:32 +0200

    Fix error _intrastat_file_available_langs

--- intrastat_product_hscodes_import/wizards/intrastat_hscodes_import_installer.py
+++ intrastat_product_hscodes_import/wizards/intrastat_hscodes_import_installer.py
@@ -12,17 +12,17 @@ from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 from odoo.osv.expression import OR
 
+CHUNK_SIZE = 65536
+
 
 def get_encoding(filename):
     detector = chardet.UniversalDetector()
-    final_chunk = False
-    blk_size = 65536
     with open(filename, "rb") as fid:
-        while (not final_chunk) and (not detector.done):
-            chunk = fid.read(blk_size)
-            if len(chunk) < blk_size:
-                final_chunk = True
+        while not detector.done:
+            chunk = fid.read(CHUNK_SIZE)
             detector.feed(chunk)
+            if len(chunk) < CHUNK_SIZE:
+                break
     detector.close()
     encoding = detector.result.get("encoding", None)
     return encoding
@@ -36,12 +36,25 @@ class IntrastatHSCodesImportInstaller(models.TransientModel):
     share_codes = fields.Boolean(
         default=True,
         help="Set this flag to share the Intrastat Codes between all "
-        "Legal Entities defined in this Odoo Database.\n",
+             "Legal Entities defined in this Odoo Database.\n",
     )
     company_id = fields.Many2one(
         comodel_name="res.company",
         string="Company",
     )
+
+    @api.onchange('share_codes')
+    def _onchange_share_codes(self):
+        for record in self:
+            if not record.share_codes and record.company_id:
+                record.company_id = False
+
+    def _intrastat_file_available_langs(self, available_lang_recs):
+        langs = self.env["res.lang"].search([('active', '=', True)]).mapped('code')
+        res = [x for x in langs if x not in available_lang_recs]
+        if res:
+            return res
+        return [_('Not installed any language')]
 
     def _hscodes_vals_domain(self):
         domain = [("company_id", "=", False)]
@@ -74,6 +87,8 @@ class IntrastatHSCodesImportInstaller(models.TransientModel):
 
     def execute(self):
         res = super().execute()
+        available_lang_recs = []
+        lang_found = False
         # get path for intrastat hs codes files
         module = __name__.split("addons.")[1].split(".")[0]
         module_path = ""
@@ -83,35 +98,34 @@ class IntrastatHSCodesImportInstaller(models.TransientModel):
                 break
         module_path += os.sep + "static/data" + os.sep
         # load existing intrastat codes
-        hs_codes = self.env["hs.code"].search(self._hscodes_vals_domain())
-        hscodes_lookup = {}
-        for i, c in enumerate(hs_codes):
-            hscodes_lookup[c.local_code] = i
-        # load csv files from data
-        lang_found = False
-        CN_fns = os.listdir(module_path)
-        langs = {x[5:7] for x in CN_fns}
-        for lang in langs:
-            lang_recs = self.env["res.lang"].search([("code", "=like", lang + "_%")])
-            if not lang_recs:
-                continue
-            lang_found = True
-            CN_fn = [x for x in CN_fns if x[5:7] == lang][0]
-            encoding = get_encoding(module_path + CN_fn)
-            with io.open(module_path + CN_fn, mode="r", encoding=encoding) as CN_file:
-                intrastat_codes = csv.DictReader(CN_file, delimiter=";")
-                for lang_rec in lang_recs:
-                    hs_codes = hs_codes.with_context(lang=lang_rec.code)
-                    for row in intrastat_codes:
-                        hs_codes, hscodes_lookup = self._load_code(
-                            row, hs_codes, hscodes_lookup
-                        )
+        for record in self:
+            hs_codes = self.env["hs.code"].search(record._hscodes_vals_domain())
+            hscodes_lookup = {}
+            for i, c in enumerate(hs_codes):
+                hscodes_lookup[c.local_code] = i
+            # load csv files from data
+            CN_fns = os.listdir(module_path)
+            langs = {x[5:7] for x in CN_fns}
+            for lang in langs:
+                lang_recs = self.env["res.lang"].search([("code", "=like", lang + "_%")])
+                available_lang_recs += lang_recs.mapped('code')
+                if not lang_recs:
+                    continue
+                lang_found = True
+                CN_fn = [x for x in CN_fns if x[5:7] == lang][0]
+                encoding = get_encoding(module_path + CN_fn)
+                with io.open(module_path + CN_fn, mode="r", encoding=encoding) as CN_file:
+                    intrastat_codes = csv.DictReader(CN_file, delimiter=";")
+                    for lang_rec in lang_recs:
+                        hs_codes = hs_codes.with_context(lang=lang_rec.code)
+                        for row in intrastat_codes:
+                            hs_codes, hscodes_lookup = record._load_code(row, hs_codes, hscodes_lookup)
         if not lang_found:
             raise UserError(
                 _(
                     "None of the languages active on your system are "
                     "available in intrastat codes files [%s]"
                 )
-                % ",".join(self._intrastat_file_available_langs())
+                % ",".join(self._intrastat_file_available_langs(available_lang_recs))
             )
         return res

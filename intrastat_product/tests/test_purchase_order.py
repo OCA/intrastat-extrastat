50f9a9f21843885f3772a634f865b0ddd26580ab
Alexis de Lattre <alexis.delattre@akretion.com>
2024-01-24 00:28:57 +0100

    intrastat_product: add confirmed state
    Add a third step "confirmed" between "draft" and "done". It is necessary
    to have error messages of XML file generation that refer to declaration
    lines.
    
    Add line_number on declaration line, to be used in error message when
    generating XML file.
    
    note field moved from fields.Text to fields.Html
    
    Improve display_name of intrastat.product.declaration
    
    Add store=True on 3 computed fields (reporting_level on
    declaration, src_dest_country_code and product_origin_country_code on
    computation lines). It fixes a bug on src_dest_country_code and
    product_origin_country_code on computation lines, when you could not set
    manually a country code while leaving the m2o field empty.

--- intrastat_product/tests/test_purchase_order.py
+++ intrastat_product/tests/test_purchase_order.py
@@ -45,7 +45,8 @@ class TestIntrastatProductPurchase(IntrastatPurchaseCommon):
         self.declaration.action_gather()
 
         self._check_line_values()
-        self.declaration.done()
+        self.declaration.draft2confirmed()
+        self.declaration.confirmed2done()
         self._check_line_values(final=True)
 
         # Check the Excel file

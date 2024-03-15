1e78f279479ae7a73c049d7245ee577b8e1552d3
Andreu Orensanz <andreu.orensanz@forgeflow.com>
2024-03-15 12:31:06 +0100

    [MIG] product_harmonized_system_delivery: Migration to 17.0

--- product_harmonized_system_delivery/__manifest__.py
+++ product_harmonized_system_delivery/__manifest__.py
@@ -4,14 +4,14 @@
 
 {
     "name": "Product Harmonized System Codes - Delivery",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Reporting",
     "license": "AGPL-3",
     "summary": "Hide native hs_code field provided by the delivery module",
     "author": "Akretion, Odoo Community Association (OCA)",
     "maintainers": ["alexis-via", "luc-demeyer"],
     "website": "https://github.com/OCA/intrastat-extrastat",
-    "depends": ["delivery", "product_harmonized_system"],
+    "depends": ["stock_delivery", "product_harmonized_system"],
     "data": ["views/product_template.xml"],
     "installable": True,
     "auto_install": True,
--- product_harmonized_system_delivery/models/product_template.py
+++ product_harmonized_system_delivery/models/product_template.py
@@ -7,5 +7,6 @@ from odoo import fields, models
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
+    # IMPORTANT: Overwrites two Odoo standard fields
     hs_code = fields.Char(related="hs_code_id.hs_code", store=True)
     country_of_origin = fields.Many2one(related="origin_country_id", store=True)

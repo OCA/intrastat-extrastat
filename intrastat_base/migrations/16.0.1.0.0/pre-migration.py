b8a9a33e6f36ba40b4b7f15cc57c1cd166d0ea39
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2024-05-27 14:37:01 +0200

    [OU-ADD] intrastat_base: Migration scripts
    Handle the change from boolean to selection on `instrastat` field
    in account.fiscal.position.
    
    TT49367

---
+++ intrastat_base/migrations/16.0.1.0.0/pre-migration.py
@@ -0,0 +1,10 @@
+# Copyright 2024 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.rename_columns(
+        env.cr, {"account_fiscal_position": [("intrastat", None)]}
+    )

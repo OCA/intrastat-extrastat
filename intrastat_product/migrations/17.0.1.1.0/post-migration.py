966512dfa669a2e0c3e6a0b7fbc5c11645ca4b9a
GuillemCForgeFlow <guillem.casassas@forgeflow.com>
2024-11-07 12:33:11 +0100

    [FIX]intrastat_product: use env in openupgrade `load_data` instead of cr As stated in https://github.com/OCA/openupgradelib/blob/master/openupgradelib/openupgrade.py#L297, the param should `env`, instead of the cursor

--- intrastat_product/migrations/17.0.1.1.0/post-migration.py
+++ intrastat_product/migrations/17.0.1.1.0/post-migration.py
@@ -6,5 +6,5 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.load_data(
-        env.cr, "intrastat_product", "migrations/17.0.1.1.0/noupdate_changes.xml"
+        env, "intrastat_product", "migrations/17.0.1.1.0/noupdate_changes.xml"
     )

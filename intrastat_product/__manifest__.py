e4fb8592de4c7be120642b87b4b3f565c6c2183c
mymage <stefano.consolaro@mymage.it>
2024-11-26 17:11:13 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (277 of 277 strings)
    
    Translation: intrastat-extrastat-17.0/intrastat-extrastat-17.0-intrastat_product
    Translate-URL: https://translation.odoo-community.org/projects/intrastat-extrastat-17-0/intrastat-extrastat-17-0-intrastat_product/it/

--- intrastat_product/__manifest__.py
+++ intrastat_product/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "Intrastat Product",
-    "version": "17.0.1.1.0",
+    "version": "17.0.1.1.1",
     "category": "Intrastat",
     "license": "AGPL-3",
     "summary": "Base module for Intrastat Product",

94d34ac58287c6e58701f6f9b28165761238d27f
Alexis de Lattre <alexis.delattre@akretion.com>
2025-02-06 12:05:43 +0100

    [IMP] intrastat_base: add field intrastat_type on product.template

--- intrastat_base/models/product_template.py
+++ intrastat_base/models/product_template.py
@@ -9,27 +9,45 @@ from odoo.exceptions import ValidationError
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
+    intrastat_type = fields.Selection(
+        [
+            ("product", "Product"),
+            ("service", "Service"),
+        ],
+        compute="_compute_intrastat_type",
+        store=True,
+        help="Type of product used for the intrastat declarations.",
+    )
     is_accessory_cost = fields.Boolean(
         help="Activate this option for shipping costs, packaging "
         "costs and all services related to the sale of products. "
         "This option is used for Intrastat reports.",
     )
 
-    @api.constrains("type", "is_accessory_cost")
+    @api.depends("type", "combo_ids.combo_item_ids.product_id.type")
+    def _compute_intrastat_type(self):
+        for this in self:
+            intrastat_type = "service"
+            if this.type == "consu":
+                intrastat_type = "product"
+            elif this.type == "combo":
+                for combo in this.combo_ids:
+                    for item in combo.combo_item_ids:
+                        if item.product_id.type == "consu":
+                            intrastat_type = "product"
+                            break
+            this.intrastat_type = intrastat_type
+
+    @api.constrains("intrastat_type", "is_accessory_cost")
     def _check_accessory_cost(self):
         for this in self:
-            if this.is_accessory_cost and this.type != "service":
+            if this.is_accessory_cost and this.intrastat_type != "service":
                 raise ValidationError(
                     _(
-                        "The option 'Is accessory cost?' should only be "
+                        "The option 'Is accessory cost?' can only be "
                         "activated on 'Service' products. You have activated "
                         "this option for the product '%(product_name)s' which is "
-                        "configured with type '%(product_type)s'."
+                        "not a service product.",
+                        product_name=this.display_name,
                     )
-                    % {
-                        "product_name": this.display_name,
-                        "product_type": this._fields["type"].convert_to_export(
-                            this.type, this
-                        ),
-                    }
                 )
